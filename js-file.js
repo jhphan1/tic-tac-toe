cd34090080b64f4beeda2b7e8666d179428014ab
Jason <jhphan1@gmail.com>
2022-03-03 12:39:56 -0800

    Add gameFlow module, allowing players to alternate turns picking squares

--- js-file.js
+++ js-file.js
@@ -11,9 +11,15 @@ const gameboard = (() => {
     // Link each array element to DOM square with textContent
     const squares = document.querySelectorAll(".container div");
 
-    for (let i = 0; i < array.length; i++) {
-        squares[i].textContent = array[i];
-    };
+    const refresh = () => {
+        for (let i = 0; i < array.length; i++) {
+            squares[i].textContent = array[i];
+        };
+    }
+
+    refresh();
+
+    return {array, refresh};
 })();
 
 
@@ -22,15 +28,37 @@ function makePlayer (name, marker) {
     return {name, marker};
 };
 
+// Pre-fill some players 
+const playerOne = makePlayer("Anna", "X");
+const playerTwo = makePlayer("Jason", "O");
+
+
 // Game Flow module
+const gameFlow = (() => {
 //     Players alternate turns
+    let currentTurn = playerOne;
 
-// Display Controller module
-//     displayWinner()
+    const squares = document.querySelectorAll(".container div");
+    squares.forEach(square => square.addEventListener("click", addMarker));
 
+    function addMarker() {
+        // Add current player's marker to square
+        gameboard.array[this.id] = currentTurn.marker;
+        gameboard.refresh();
 
-// Function: add mark to square
+        // Next player's turn
+        currentTurn = (currentTurn === playerOne) ? playerTwo : playerOne;
+    };
+
+//     Function: add mark to square
 //     Player clicks on square
 //     Player's mark is added to array
 //     DOM is updated with mark
 //     Error: Can't add to pre-filled square
+
+})();
+
+
+
+// Display Controller module
+//     displayWinner()
